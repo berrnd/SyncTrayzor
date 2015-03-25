9634d9643555ad548a358344d529893b2240b96b
Antony Male <antony.male@gmail.com>
2015-03-25 21:54:13 +0000

    More styling the expanders

---
+++ src/SyncTrayzor/Xaml/DisplayLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTrayzor.Xaml
+{
+    public class DisplayLine
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+    }
+}
